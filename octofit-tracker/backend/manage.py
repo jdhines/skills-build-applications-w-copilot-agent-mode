70c24e0e746954dee815721976206a85efe35c1a
orangehat <jessdhines@gmail.com>
2025-04-08 23:19:50 +0000

    commit to try to get to the next step even though the app won't start

--- octofit-tracker/backend/manage.py
+++ octofit-tracker/backend/manage.py
@@ -9,12 +9,12 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "octofit_tracker.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ModuleNotFoundError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        "Couldn't import Django. Make sure it's installed and "
+        "available on your PYTHONPATH environment variable. Did you "
+        "forget to activate a virtual environment?"
+            ) from exc
     execute_from_command_line(sys.argv)
 
 
--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-tl4#*+bid8evktt8sj5t+5fezwtmd-&-^tg4gvdxyth5d&l0p2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'orange-engine-5455pqw455h4xx6.-8000.app.github.dev']
 
 
 # Application definition
